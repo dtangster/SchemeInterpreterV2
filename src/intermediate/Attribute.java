707813052f6e02fa3f02546acc36cbdc3d1b4cb1
David Tang <dtangster@yahoo.com>
2013-11-20 01:49:38 -0800

    Fixed a comment.

--- src/intermediate/Attribute.java
+++ src/intermediate/Attribute.java
@@ -1,6 +1,6 @@
 package intermediate;
 
 public enum Attribute {
-    DEFINE_NODE, // Root node of that the variable name refers to
+    DEFINE_NODE, // Root node of the node that the variable name refers to
     NUMBER_CONSTANT,
 }
