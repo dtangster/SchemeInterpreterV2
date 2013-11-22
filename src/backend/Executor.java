cf018dcfb3ace62b2a6757d875e79e261d0ed38d
thaesulatt <thaesulatt@gmail.com>
2013-11-21 17:49:12 -0800

    ignore defining procedures and check if lambda then ignore

--- src/backend/Executor.java
+++ src/backend/Executor.java
@@ -30,34 +30,47 @@ public class Executor {
                 executeProcedure(topLevelTable, node);
             }
 
-            // If defining a constant, then add it to the runtime stack
-            else {
-                String variableId = node.getCdr().getToken().getText(); // Get name of variable
-                SymTabEntry entry = topLevelTable.getEntry(variableId); // Get corresponding SymTabEntry
-                Node variable = (Node) entry.get(Attribute.VARIABLE_NODE);
+            String variableId = node.getCdr().getToken().getText(); // Get name of variable
+            SymTabEntry entry = topLevelTable.getEntry(variableId); // Get corresponding SymTabEntry
+            Node variable = (Node) entry.get(Attribute.VARIABLE_NODE);
 
-                // Go into this block to determine if the variable refers to a procedure or constant
-                if (variable != null) {
-                    Node tempNode;
-                    SymTabEntry tempEntry;
+           // ignore defining procedures
+            TokenType type = variable.getToken().getType();
+
+            //check if lambda then ignore
+            if(type!= TokenType.KW_LAMBDA){continue;}
+
+            //set values to x, y, main AR
+            else if (entry.get(Attribute.NUMBER_CONSTANT) != null) {
+                    SymTabEntry newEntry = runTimeStack.enterLocal(variableId);
+                    newEntry.putAll(entry); // Copy the Attributes from the original SymTabEntry (including the value)
+                }
+            }
+        }
+
+    public void executeProcedure(SymTab topLevelTable, Node node) {
+
+        // If defining a constant, then add it to the runtime stack
+
+            String variableId = node.getCdr().getToken().getText(); // Get name of variable
+            SymTabEntry entry = topLevelTable.getEntry(variableId); // Get corresponding SymTabEntry
+            Node variable = (Node) entry.get(Attribute.VARIABLE_NODE);
+
+            // Go into this block to determine if the variable refers to a procedure or constant
+            if (variable != null) {
+                Node tempNode;
+                SymTabEntry tempEntry;
+
 
                     while (variable != null) {
+
                         //TODO: Figure out if the variable refers to a procedure of constant.
                         //TODO: If it is a constant, add it to the top level symbol table. Otherwise, do nothing.
                         //TODO: Keep in mind that a VARIABLE_NODE can refer to a VARIABLE_NODE and etc. We must
                         //TODO: iterate through all VARIABLE_NODE Attributes until it returns null. Then we have
                         //TODO: to check if it is a constant of procedure.
                     }
-                }
-                else if (entry.get(Attribute.NUMBER_CONSTANT) != null) {
-                    SymTabEntry newEntry = runTimeStack.enterLocal(variableId);
-                    newEntry.putAll(entry); // Copy the Attributes from the original SymTabEntry (including the value)
-                }
             }
-        }
-    }
-
-    public void executeProcedure(SymTab topLevelTable, Node node) {
 
     }
 }
